26f81c3c43746add71b09af914c57edf074edf1e
andrehaffner <andrehaffner@outlook.com>
2023-06-26 16:39:13 -0700

    removed blank line

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -5,6 +5,5 @@ from flask import Flask
 def create_app():
     app = Flask(__name__, template_folder='../templates', static_folder="../static")
     app.config["SECRET_KEY"] = "super_secret_key"
-
     app.register_blueprint(Views, url_prefix="/")
     return app
